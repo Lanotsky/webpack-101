ad27915e22be0c4c4e1613ece4bcb855a1af286f
Aaron James Lanot <aaronlanot@gmail.com>
2018-05-23 13:56:39 +0800

    added browsersync

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
 
 module.exports={
     mode: 'development',
@@ -26,16 +27,19 @@ module.exports={
       }
     },
     devtool: "source-map", 
+    devServer: {
+      contentBase: './dist'
+    },
     module: {
       rules: [
         {
           test: /\.css$/,
-          use: [ MiniCssExtractPlugin.loader ,'css-loader','postcss-loader'],
+          use: [  MiniCssExtractPlugin.loader ,'css-loader','postcss-loader'],
         },
         {
           test: /\.scss$/,
           use: [
-          {loader: MiniCssExtractPlugin.loader}, // can fallback to 'style-loader'
+          {loader:  MiniCssExtractPlugin.loader}, // MiniCssExtractPlugin.loader can fallback to 'style-loader'
           {loader: "css-loader", options:{'sourceMap': true}}, 
           {loader: 'postcss-loader',options:{'sourceMap': true}},
           {
@@ -57,11 +61,17 @@ module.exports={
     plugins: [
       new CleanWebpackPlugin(['dist']),
       new HtmlWebpackPlugin({
-        title: 'output management'
+        title: 'Webpack-101'
       }),
       new MiniCssExtractPlugin({
         filename: "styles.css",
+      }),
+      new BrowserSyncPlugin({
+        host: 'localhost',
+        port: 3000,
+        proxy: 'http://localhost:8080/'
       })
+
     ]
     
 }
